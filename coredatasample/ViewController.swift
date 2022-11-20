31813d29f9eeadef47620e6b0378f96768d73b2e
Fernando Salom <Fdosalom@gmail.com>
2022-11-20 11:46:01 +0100

    Add initial project 📦

---
+++ coredatasample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coredatasample
+//
+//  Created by Fernando Salom Carratala on 20/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
